fc62d7fb74255edd3e22433a69d90331e0433894
bartuom <rbartek1996@gmail.com>
2026-08-18 23:19:10 +0200

    P3.8.1 load updated Environment emitter

--- site/fxdeck/effects/catalog.js
+++ site/fxdeck/effects/catalog.js
@@ -1,7 +1,7 @@
 import { registerHeavyImpact } from './heavy-impact.js?v=p3.6.0';
 import { registerExplosion } from './explosion.js?v=p3.6.0';
 import { registerFireball } from './fireball.js?v=p3.6.4';
-import { registerEnvironmentEmitter } from './environment-emitter.js?v=p3.8.0';
+import { registerEnvironmentEmitter } from './environment-emitter.js?v=p3.8.1';
 
 export function registerProductionEffects(fx) {
   registerHeavyImpact(fx);
