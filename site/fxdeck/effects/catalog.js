93c33f099d15d3dbf3eb5396970032b0389219ad
bartuom <rbartek1996@gmail.com>
2026-08-18 23:05:38 +0200

    P3.8.0 add environment emitter to production catalog

--- site/fxdeck/effects/catalog.js
+++ site/fxdeck/effects/catalog.js
@@ -1,10 +1,12 @@
 import { registerHeavyImpact } from './heavy-impact.js?v=p3.6.0';
 import { registerExplosion } from './explosion.js?v=p3.6.0';
 import { registerFireball } from './fireball.js?v=p3.6.4';
+import { registerEnvironmentEmitter } from './environment-emitter.js?v=p3.8.0';
 
 export function registerProductionEffects(fx) {
   registerHeavyImpact(fx);
   registerExplosion(fx);
   registerFireball(fx);
+  registerEnvironmentEmitter(fx);
   return fx;
 }
