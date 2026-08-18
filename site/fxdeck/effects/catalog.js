64f18dbeacbfa3e786f9c0ed9b7b2ecc987c2d2c
bartuom <rbartek1996@gmail.com>
2026-08-18 20:07:35 +0200

    P3.6 add production effect catalog

---
+++ site/fxdeck/effects/catalog.js
@@ -0,0 +1,10 @@
+import { registerHeavyImpact } from './heavy-impact.js?v=p3.6.0';
+import { registerExplosion } from './explosion.js?v=p3.6.0';
+import { registerFireball } from './fireball.js?v=p3.6.0';
+
+export function registerProductionEffects(fx) {
+  registerHeavyImpact(fx);
+  registerExplosion(fx);
+  registerFireball(fx);
+  return fx;
+}
