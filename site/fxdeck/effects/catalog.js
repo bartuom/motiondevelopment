80f13636f85d1e071d3e43505bc285acdbae1015
bartuom <rbartek1996@gmail.com>
2026-08-18 23:46:08 +0200

    P3.9.0 register Rare Reward effect

--- site/fxdeck/effects/catalog.js
+++ site/fxdeck/effects/catalog.js
@@ -2,11 +2,13 @@ import { registerHeavyImpact } from './heavy-impact.js?v=p3.6.0';
 import { registerExplosion } from './explosion.js?v=p3.6.0';
 import { registerFireball } from './fireball.js?v=p3.6.4';
 import { registerEnvironmentEmitter } from './environment-emitter.js?v=p3.8.1';
+import { registerRareReward } from './rare-reward.js?v=p3.9.0';
 
 export function registerProductionEffects(fx) {
   registerHeavyImpact(fx);
   registerExplosion(fx);
   registerFireball(fx);
   registerEnvironmentEmitter(fx);
+  registerRareReward(fx);
   return fx;
 }
