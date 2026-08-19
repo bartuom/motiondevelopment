aace349ddb4aa0c0f7c36e001bb934ac07c19549
bartuom <rbartek1996@gmail.com>
2026-08-20 00:02:20 +0200

    P4.4.1 cache-bust schema validator dependency

--- site/fxdeck/schema/effect-loader.js
+++ site/fxdeck/schema/effect-loader.js
@@ -1,4 +1,4 @@
-import { assertValidEffectDefinition } from './validator.js?v=p4.4.0';
+import { assertValidEffectDefinition } from './validator.js?v=p4.4.1';
 
 function splitOptions(options = {}) {
   const { assetManager = null, validationOptions = null, ...legacyValidationOptions } = options ?? {};
