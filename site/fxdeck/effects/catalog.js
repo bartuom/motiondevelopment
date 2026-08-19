ae00d6fd4f741d7b2035f1a545970c0ffc101561
bartuom <rbartek1996@gmail.com>
2026-08-19 11:42:18 +0200

    P3.12.0 register Critical Hit and Magic Burst in production catalog

--- site/fxdeck/effects/catalog.js
+++ site/fxdeck/effects/catalog.js
@@ -4,6 +4,8 @@ import { registerFireball } from './fireball.js?v=p3.6.4';
 import { registerEnvironmentEmitter } from './environment-emitter.js?v=p3.8.1';
 import { registerRareReward } from './rare-reward.js?v=p3.9.0-r2';
 import { registerFootballCardReveal } from './football-card-reveal.js?v=p3.10.0';
+import { registerCriticalHit } from './critical-hit.js?v=p3.11.0';
+import { registerMagicBurst } from './magic-burst.js?v=p3.12.0';
 
 export function registerProductionEffects(fx) {
   registerHeavyImpact(fx);
@@ -12,5 +14,7 @@ export function registerProductionEffects(fx) {
   registerEnvironmentEmitter(fx);
   registerRareReward(fx);
   registerFootballCardReveal(fx);
+  registerCriticalHit(fx);
+  registerMagicBurst(fx);
   return fx;
 }
