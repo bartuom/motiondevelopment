b2c938d267c8437e386f8192252d2de8c304947c
bartuom <rbartek1996@gmail.com>
2026-08-20 20:48:03 +0200

    P4.5 load effects with current validator

--- site/fxdeck/schema/effect-loader.js
+++ site/fxdeck/schema/effect-loader.js
@@ -1,4 +1,4 @@
-import { assertValidEffectDefinition } from './validator.js?v=p4.4.1';
+import { assertValidEffectDefinition } from './validator.js?v=p4.5.0';
 
 function splitOptions(options = {}) {
   const { assetManager = null, validationOptions = null, ...legacyValidationOptions } = options ?? {};
